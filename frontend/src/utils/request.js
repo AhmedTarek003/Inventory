c4f63800d8bd8ba726fc607a024f1941c8abcce1
ahemd003 <at3616464@gmail.com>
2024-03-03 15:14:20 +0200

    change

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const request = axios.create({
-  baseURL: "https://inventory-xi-five.vercel.app/api/",
+  baseURL: "https://inventory-one-self.vercel/api/",
 });
