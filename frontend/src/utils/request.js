6898178497915c32771ea1b664436a414214dc29
ahemd003 <at3616464@gmail.com>
2024-03-13 16:42:57 +0200

    update

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const request = axios.create({
-  baseURL: "https://inventory-one-self.vercel/api/",
+  baseURL: "https://inventory-ttk7.onrender.com/api/",
 });
